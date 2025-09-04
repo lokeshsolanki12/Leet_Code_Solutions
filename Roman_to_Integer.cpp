a84f9180f710cee47cf6fc227d0c6e36c20def1a
Lokesh Solanki <lokesh.solanki7014.in>
2025-09-04 23:13:26 +0530

    Code update

---
+++ Roman_to_Integer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
+        
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && roman[s[i]] < roman[s[i + 1]]) {
+                result -= roman[s[i]];  
+            } else {
+                result += roman[s[i]];
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.romanToInt("III") << endl;     
+    cout << sol.romanToInt("LVIII") << endl;    
+    cout << sol.romanToInt("MCMXCIV") << endl;  
+    return 0;
+}
